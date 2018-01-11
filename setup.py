3a8273150b00f321393d1aaed18967beb2a56b77
Victor Gavro <vgavro@gmail.com>
2018-01-11 19:31:18 +0200

    (NOTE: not to merge!) changed version from -dev to avoid setuptools conflict problems

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Flask-SQLAlchemy',
-    version='3.0-dev',
+    version='3.0',
     url='http://github.com/mitsuhiko/flask-sqlalchemy',
     license='BSD',
     author='Armin Ronacher',
